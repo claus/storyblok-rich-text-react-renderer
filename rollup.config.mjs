3d562a4c5d893f1dfd8fbc8b6629301052bf38e6
Claus Wahlers <claus@codeazur.com.br>
2025-05-14 18:18:18 -0300

    Add support for table, tableHeader, tableRow, tableCell node types (#53)
    * Implement table, tableHeader, tableRow, tableCell node types
    * Prettier formatting
    * Add types related to table
    * Update README, update dependencies, bump version to 3.0.1

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,25 +1,23 @@
-import babel from "@rollup/plugin-babel";
-import commonjs from "@rollup/plugin-commonjs";
-import external from "rollup-plugin-peer-deps-external";
-import { readFile } from "fs/promises";
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import { readFile } from 'fs/promises';
 
-const pkg = JSON.parse(
-  await readFile(new URL("./package.json", import.meta.url))
-);
+const pkg = JSON.parse(await readFile(new URL('./package.json', import.meta.url)));
 
 export default {
-  input: "src/index.js",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      sourcemap: true,
-    },
-    {
-      file: pkg.module,
-      format: "es",
-      sourcemap: true,
-    },
-  ],
-  plugins: [babel({ babelHelpers: "bundled" }), external(), commonjs()],
+    input: 'src/index.js',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+            sourcemap: true,
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            sourcemap: true,
+        },
+    ],
+    plugins: [babel({ babelHelpers: 'bundled' }), external(), commonjs()],
 };
